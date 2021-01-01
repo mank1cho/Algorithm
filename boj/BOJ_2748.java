13bd5744d600e8808cf60555ca1baa9a2dcf30a5
mank1cho <jjangman821@gmail.com>
2021-01-01 21:38:49 +0900

    2021-01-01

---
+++ boj/BOJ_2748.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.io.*;
+
+public class BOJ_2748 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+				
+		long a = 0, b = 1, c = 0;
+		for(int i = 1; i<N; ++i) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		if(N == 1) System.out.print(1);
+		else System.out.print(c);
+	}
+}
