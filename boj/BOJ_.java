0813fb063365a688e810d496476f4b511a2ddb54
mank1cho <jjangman821@gmail.com>
2021-01-19 22:45:10 +0900

    2021-01-19

--- boj/BOJ_.java
+++ boj/BOJ_.java
@@ -9,5 +9,12 @@ public class BOJ_ {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while(true) {
+			st.nextToken().split(",");
+			int x = Integer.parseInt(st.nextToken());
+			if(x==0) break;
+		}
+		
 	}
 } 
--- boj/BOJ_10216.java
+++ boj/BOJ_10216.java
@@ -5,7 +5,7 @@ package boj;
 import java.io.*;
 import java.util.*;
 
-public class BOJ_10216 {
+public class BOJ_10216 {	
 	public static class INFO{
 		int x, y, r;
 		INFO(int x, int y, int r){
@@ -14,12 +14,18 @@ public class BOJ_10216 {
 			this.r = r;
 		}
 	}
+	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		while(T-->0) {
 			int N = Integer.parseInt(br.readLine());
-			ArrayList<Integer>[] adj = new ArrayList()[];
+			
+			int[] p = new int[N];
+			for(int i = 0; i<N; ++i) {
+				p[i] = i;
+			}
 			
 			INFO[] arr = new INFO[N];
 			for(int i = 0; i<N; ++i) {
@@ -29,12 +35,33 @@ public class BOJ_10216 {
 				int r = Integer.parseInt(st.nextToken());
 				arr[i] = new INFO(x,y,r);
 				for(int j = 0; j<i; ++j) {
-					
+					int rdis = pow(arr[i].r+arr[j].r);
+					int pdis = pow(arr[i].x-arr[j].x)+pow(arr[i].y-arr[j].y);
+					if(pdis<=rdis) {
+						p[find(p, i)] = find(p, j);
+					}
 				}
 			}
 			
+			int cnt = 0;
+			for(int i = 0; i<N; ++i) {
+				if(p[i] == i) ++cnt;
+			}
 			
+			sb.append(cnt).append('\n');
 			
 		}
+		System.out.print(sb);
 	}
-} 
+	
+	public static int find(int[] p, int x) {
+		while(p[x] != x) {
+			x = p[x];
+		}
+		return x;
+	}
+	
+	public static int pow(int x) {
+		return x*x;
+	}
+}
