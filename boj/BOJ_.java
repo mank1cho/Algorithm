02056afd8afbf654708790922921749166fe535f
mank1cho <jjangman821@gmail.com>
2021-01-19 22:46:35 +0900

    2021-01-19

--- boj/BOJ_.java
+++ boj/BOJ_.java
@@ -8,13 +8,6 @@ import java.util.*;
 public class BOJ_ {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(true) {
-			st.nextToken().split(",");
-			int x = Integer.parseInt(st.nextToken());
-			if(x==0) break;
-		}
 		
 	}
 } 
