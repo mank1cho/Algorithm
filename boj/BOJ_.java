f900f471ee712a8a45c1a49f717ed6172a7e1c11
mank1cho <jjangman821@gmail.com>
2021-01-20 22:14:21 +0900

    2021-01-20

--- boj/BOJ_.java
+++ boj/BOJ_.java
@@ -8,6 +8,6 @@ import java.util.*;
 public class BOJ_ {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 	}
 } 
