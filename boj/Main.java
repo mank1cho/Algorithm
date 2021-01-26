f623782a7a8cca2c3bfe5226c89c75db1ca09db2
mank1cho <jjangman821@gmail.com>
2021-01-26 20:34:36 +0900

    2021-01-26

--- boj/Main.java
+++ boj/Main.java
@@ -1,43 +1,13 @@
+// https://www.acmicpc.net/problem/
+// 
 package boj;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	   String s = br.readLine();
-	   
-	   int[] arr = new int[26];
-	   
-	   for(int i = 0; i<s.length(); ++i) {
-		   arr[s.charAt(i)-'A']++;
-	   }
-	   
-	   int cnt = 0;
-	   for(int i = 0; i<26; ++i) {
-		   if(arr[i]%2 == 1) cnt++;
-	   }
-	   
-	   StringBuilder sb = new StringBuilder();
-	   if(cnt > 1) System.out.println("I'm Sorry Hansoo");
-	   else {
-		   char c = '0';
-		   for(int i = 0; i<26; ++i) {
-			   if(arr[i]%2 == 1) c = (char) (i + 'A'); 
-			   for(int j = 0; j<arr[i]/2; ++j) {
-				   sb.append((char) (i + 'A'));
-			   }
-		   }
-		   if(c!='0') sb.append(c);
-		   for(int i = 25; i>=0; --i) {
-			   for(int j = 0; j<arr[i]/2; ++j) {
-				   sb.append((char) (i + 'A'));
-			   }
-		   }
-		   System.out.println(sb);
-	   }
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 	}
-}
+} 
