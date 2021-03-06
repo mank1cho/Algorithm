936e23deb4189f11f20c852c0e03e1c7a7fc358a
unknown <jjangman821@gmail.com>
2021-03-06 19:39:07 +0900

    2021-03-06

--- boj/BOJ_.java
+++ boj/BOJ_.java
@@ -8,9 +8,34 @@ import java.util.*;
  
 public class BOJ_ {
  
+	static int N, M, ans, cnt;
+	static int[] arr;
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; ++i) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
         
+        dfs(0, 0);
+        System.out.println("cnt: " + cnt);
+        System.out.println(ans);
+    }
+    
+    public static void dfs(int i, int sum) {
+    	++cnt;
+    	if(i==N) return;
+    	dfs(i+1, sum);
+    	if(sum+arr[i]<M) {
+    		dfs(i+1, sum+arr[i]);
+    	}
+    	else if(sum+arr[i] == M) {
+    		++ans;
+    	}
+    	else return;
     }
 }
